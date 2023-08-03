76db26e3daa532f1573a5193af1204222cbbda31
titantooth <ali.rabieeie@gmail.com>
2023-08-03 21:52:45 +0330

    added rest of css added CTA section

--- gpt3clone/src/component/cta/CTA.jsx
+++ gpt3clone/src/component/cta/CTA.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./cta.css";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <div className="gpt3-cta">
+      <div className="gpt3-cta-content">
+        <p>request early access</p>
+        <h3>register today and start exploring the endless possibilities.</h3>
+      </div>
+      <div className="gpt3-cta-button">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default CTA;
--- gpt3clone/src/containers/possibility/Possibility.jsx
+++ gpt3clone/src/containers/possibility/Possibility.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./possibility.css";
+import possibilityImage from "../../assets/possibility.png";
 
 const Possibility = () => {
-  return <div>Possibility</div>;
+  return (
+    <div className="gpt3-possibility section-padding" id="possibility">
+      <div className="gpt3-possibility-image">
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className="gpt3-possibility-content">
+        <h4>Request early access to get started</h4>
+        <h1 className="gradient-text">
+          the possibilities are beyond our imagination
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum iusto
+          totam, ad molestias nulla excepturi incidunt hic fugit delectus!
+          Repellendus a quae dolor architecto soluta earum iure beatae quia
+          delectus?
+        </p>
+        <h4>Request early access to get started</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Possibility;
