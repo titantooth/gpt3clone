0b0d7b32011750f03141b2cbd409406619c2d238
titantooth <ali.rabieeie@gmail.com>
2023-08-03 22:36:13 +0330

    article section and added css code to bothcomps

--- gpt3clone/src/component/article/Article.jsx
+++ gpt3clone/src/component/article/Article.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./article.css";
 
-const Article = () => {
-  return <div>Article</div>;
+const Article = ({ imgUrl, date, title }) => {
+  return (
+    <div className="gpt3-blog-container-article">
+      <div className="gpt3-blog-container-article-image">
+        <img src={imgUrl} alt="blog image" />
+      </div>
+      <div className="gpt3-blog-container-article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>read full article</p>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
--- gpt3clone/src/containers/blog/Blog.jsx
+++ gpt3clone/src/containers/blog/Blog.jsx
@@ -1,8 +1,49 @@
 import React from "react";
 import "./blog.css";
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports";
+import Article from "../../component/article/Article";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="gpt3-blog section-padding" id="blog">
+      <div className="gpt3-blog-heading">
+        <h1 className="gradient-text">
+          a lot is happening we are blogging about it
+        </h1>
+      </div>
+      <div className="gpt3-blog-container">
+        <div className="gpt3-blog-container-groupA">
+          <Article
+            imgUrl={blog01}
+            date="sep 7 2023"
+            title="some title will be placed here!!"
+          />
+        </div>
+        <div className="gpt3-blog-container-groupB">
+          <Article
+            imgUrl={blog02}
+            date="sep 7 2023"
+            title="some title will be placed here!!"
+          />
+          <Article
+            imgUrl={blog03}
+            date="sep 7 2023"
+            title="some title will be placed here!!"
+          />
+          <Article
+            imgUrl={blog04}
+            date="sep 7 2023"
+            title="some title will be placed here!!"
+          />
+          <Article
+            imgUrl={blog05}
+            date="sep 7 2023"
+            title="some title will be placed here!!"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
